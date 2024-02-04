ed9d11c58cca722d2f622fe8bf246b01c3229b1a
dingzeyuan <zdin0570@uni.sydney.edu.au>
2024-02-04 17:57:35 +0800

    hide the button

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -6,25 +6,25 @@ function Hero() {
 
 const containerStyle = {
     backgroundImage: `url(${waspbg})`,
-    backgroundSize: 'contain', // This will make sure the whole image is visible
+    backgroundSize: 'cover', // This will make sure the whole image is visible，could be "contain"
     backgroundRepeat: 'no-repeat', // Prevents repeating the image
     backgroundPosition: 'center',
     height: '60vh',
 };
 
-    const burgerstyle = {
-        backgroundImage: `url(${herobannerbg})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-    }
+    // const burgerstyle = {
+    //     backgroundImage: `url(${herobannerbg})`,
+    //     backgroundSize: 'cover',
+    //     backgroundPosition: 'center',
+    // }
     return (
         <div className='hero-container' style={containerStyle}>
-            <div className='hero-info'>
+            {/*<div className='hero-info'>*/}
                 {/*<h6>青蜂</h6>*/}
                 {/*<h1 className='hero-heading'>让生活充满仪式感!</h1>*/}
                 {/*<p className='hero-discription'>Food is any substance consumed to provide nutritional support for an organism.</p>*/}
-                <button className='button'>现在下单</button>
-            </div>
+                {/*<button className='button'>现在下单</button>*/}
+            {/*</div>*/}
             {/*<div  className='burger' style={burgerstyle}>*/}
             {/*    <img  src={logo2} width={500} />*/}
             {/*</div>*/}
